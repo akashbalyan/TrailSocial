b841ac9f4ac495c00ac777e936200fb68476aa67
akashbalyan <akash.balyan139600@gmail.com>
2023-11-28 00:55:06 -0800

    added backend url

--- Frontend/src/config/axios-config.js
+++ Frontend/src/config/axios-config.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const renderBackendUrl = 'https://trailback.onrender.com';
 const instance = axios.create({
-  baseURL: 'http://localhost:5001',
+  // baseURL: 'http://localhost:5001',
+  baseURL:renderBackendUrl
 });
 
 export default instance;

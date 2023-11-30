b601bceb0b8e47a05aa222c03378c6135af87376
akashbalyan <akash.balyan139600@gmail.com>
2023-11-30 03:31:09 -0800

    changed backend url to local server

--- Frontend/src/config/axios-config.js
+++ Frontend/src/config/axios-config.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const renderBackendUrl = 'https://trailback.onrender.com';
 const instance = axios.create({
-  // baseURL: 'http://localhost:5001',
-  baseURL:renderBackendUrl
+  baseURL: 'http://localhost:5001',
+  //baseURL:renderBackendUrl
 });
 
 export default instance;
